1d804e1b495e8141a3c92ed1104b9614a30a4ff9
Sambit Chakraborty <chakrabortysambit00403@gmail.com>
2026-07-22 19:33:15 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+setup(
+    name='Medical_chatbot',
+    version='0.1.0',
+    author='Sambit Chakraborty',
+    author_email='chakrabortysambit00403@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
