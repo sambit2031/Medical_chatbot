c201abe643eb76c7a81edf9893ebae9daad719f8
Sambit Chakraborty <chakrabortysambit00403@gmail.com>
2026-07-22 18:59:43 +0530

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+#create directories
+mkdir -p src
+mkdir -p research
+#create files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/trials.ipynb
+
+echo "Project structure created successfully."
